1261a3b9dc89642bb828358839bea0d7abedf909
Gaku Masuda <gals02513@gmail.com>
2024-12-20 05:05:50 +0900

    キャッシュ戦略変更

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,33 @@ const withPWA = nextPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
+  runtimeCaching: [
+    {
+      // APIリクエストをキャッシュせず、常にネットワークから取得
+      urlPattern: /^https:\/\/irina\.f5\.si\/.*$/,
+      handler: 'NetworkOnly',
+    },
+    {
+      // 静的リソースをキャッシュ
+      urlPattern: /^https:\/\/discord-music-app\.vercel\.app\/_next\/static\/.*$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'static-resources',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30日
+        },
+      },
+    },
+    // 追加のキャッシュ戦略があればここに記載
+  ],
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.ytimg.com', 'lh3.googleusercontent.com'],
-    unoptimized: true, // 追加: 画像最適化を無効化
+    unoptimized: true, // 画像最適化を無効化
   },
   async headers() {
     return [
